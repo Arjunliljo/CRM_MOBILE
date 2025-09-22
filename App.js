ad98bd117b15b22e55ffdb2a600313a0ddb46af8
Arjunliljo <arjun7180@gmail.com>
2025-09-22 14:47:47 +0530

    chore: add @tanstack/react-query and wrap app with QueryClientProvider

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import { StatusBar } from "expo-status-bar";
 import Route from "./routes/Route";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
     <>
       <GestureHandlerRootView style={{ flex: 1 }}>
         <StatusBar style="auto" />
-        <Route />
+        <QueryClientProvider client={queryClient}>
+          <Route />
+        </QueryClientProvider>
       </GestureHandlerRootView>
     </>
   );
